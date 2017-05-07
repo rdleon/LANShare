107343be61414281bc575d1144a28a1b6684c7a8
R. Díaz de León <leon@elinter.net>
2017-05-07 12:55:52 -0500

    Copying files

--- src/lanshare.c
+++ src/lanshare.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define OK 0
+#define ERR 1
 
 int
-main(int argc, char* argv[])
+search_hosts()
 {
+	return OK;
+}
+
+int
+main(int argc, char *argv[])
+{
+	int ret = 0;
+
 	printf("Searching...\n");
+
+	ret = search_hosts();
+
+	if (ret != OK)
+		fprintf(stderr, "Couldn't find any hosts\n");
+
 	return 0;
 }
